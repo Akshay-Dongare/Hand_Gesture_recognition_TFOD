edafed4a55d0286acfca02b6671c440ae97f7e99
Akshay Dongare <75126845+Akshay-Dongare@users.noreply.github.com>
2024-05-01 02:50:24 +0530

    Add files via upload

---
+++ My Code/webcam.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+import cv2
+import numpy as np
+
+detection_model = tf.saved_model.load('.\my_ssd_mobnet\export\saved_model')
+
+print(type(detection_model))
+
+@tf.function
+def detect_fn(image):
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+    return detections
+
+
+
+##webcam
+cap = cv2.VideoCapture(0)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    image_np = np.array(frame)
+
+    input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.uint8)
+    #detections = detect_fn(input_tensor)
+
+    DEFAULT_FUNCTION_KEY = 'serving_default'
+    predict_func = detection_model.signatures[DEFAULT_FUNCTION_KEY]
+    detections = predict_func(input_tensor)
+    print(detections)
+    print(type(detections))
+
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                  for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+    label_id_offset = 1
+    image_np_with_detections = image_np.copy()
+
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+        image_np_with_detections,
+        detections['detection_boxes'],
+        detections['detection_classes'] + label_id_offset,
+        detections['detection_scores'],
+        category_index,
+        use_normalized_coordinates=True,
+        max_boxes_to_draw=5,
+        min_score_thresh=.8,
+        agnostic_mode=False)
+
+    cv2.imshow('object detection', cv2.resize(image_np_with_detections, (800, 600)))
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
+        break
